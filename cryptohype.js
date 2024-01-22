2d0b2c76e3030f305b5cbf4f88439548097dfc1f
Benjamin AUBRON <benjamin.aubron@outlook.com>
2024-01-22 23:18:48 +0100

    countdown added

---
+++ cryptohype.js
@@ -0,0 +1,43 @@
+
+
+
+function calculateCountdown (){
+  
+  const now = new Date();
+  
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+  
+  const countdownHours = 23-hours;
+  const countdownMinutes = 60-minutes;
+  const countdownSeconds = 60-seconds;
+  
+  const formattedHours = countdownHours < 10 ? `0${countdownHours}` : countdownHours;
+  const formattedMinutes = countdownMinutes < 10 ? `0${countdownMinutes}` : countdownMinutes;
+  const formattedSeconds = countdownSeconds < 10 ? `0${countdownSeconds}` : countdownSeconds;
+  
+  const finalCountdown = `${formattedHours}:${formattedMinutes}:${formattedSeconds}`
+  
+  return finalCountdown
+  
+}
+
+
+
+function updateCountdown() {
+
+  const countdown = document.querySelector(".countdown");
+
+
+  if (countdown) {
+      setInterval(function () {
+          countdown.textContent = calculateCountdown();
+      }, 1000);
+  } else {
+      console.error("Error");
+  }
+}
+
+
+updateCountdown();
