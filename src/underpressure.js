4545e7a719151f11eda9c0518a4cd0b64f2a202e
Benjamin AUBRON <benjamin.aubron@outlook.com>
2024-02-29 16:17:22 +0100

    carousel - about - done

--- src/underpressure.js
+++ src/underpressure.js
@@ -1,33 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//   // Initialisation du carousel avec le contenu de la div "about1"
-//   document.getElementById("carousel").innerText = document.getElementById("about1").innerText;
-
-//   // Écouteur d'événement pour le bouton "Left"
-//   document.getElementById("btn_left").addEventListener("click", function() {
-//       // Récupération de l'ID de la div actuellement affichée dans le carousel
-//       var currentDivId = document.getElementById("carousel").innerText.trim();
-
-//       // Récupération de l'ID de la div précédente
-//       var prevDivId = currentDivId === "about1" ? "about4" : "about" + (parseInt(currentDivId.slice(-1)) - 1);
-
-//       // Affichage du contenu de la div précédente dans le carousel
-//       document.getElementById("carousel").innerText = document.getElementById(prevDivId).innerText;
-//   });
-
-//   // Écouteur d'événement pour le bouton "Right"
-//   document.getElementById("btn_right").addEventListener("click", function() {
-//       // Récupération de l'ID de la div actuellement affichée dans le carousel
-//       var currentDivId = document.getElementById("carousel").innerText.trim();
-
-//       // Récupération de l'ID de la div suivante
-//       var nextDivId = currentDivId === "about4" ? "about1" : "about" + (parseInt(currentDivId.slice(-1)) + 1);
-
-//       // Affichage du contenu de la div suivante dans le carousel
-//       document.getElementById("carousel").innerText = document.getElementById(nextDivId).innerText;
-//   });
-// });
-
-
 
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -40,13 +10,31 @@ document.addEventListener("DOMContentLoaded", function() {
   //Click on btn_right
   document.getElementById("btn_right").addEventListener("click",function () {
     //Get the current number of the CardId in the carousel
-    const currentCardId = carousel.firstChild.id;
-    const idNumber = 
-    //Get the next CardId
-    
+    const currentCardIdNumber = carousel.firstChild.id.slice(-1);
+    //Get the next CardId 
+    const nextCardIdNumber = currentCardIdNumber == 4 ? 1 : parseInt(currentCardIdNumber) + 1;
+    const nextCard = document.getElementById("about"+nextCardIdNumber.toString())
 
     //Display the content of the next CardId
-    console.log(currentCardId)
+    while (carousel.firstChild) {
+      carousel.removeChild(carousel.firstChild);
+    }
+    carousel.appendChild(nextCard.cloneNode(true));
+  })
+
+  //Click on btn_left
+  document.getElementById("btn_left").addEventListener("click",function () {
+    //Get the current number of the CardId in the carousel
+    const currentCardIdNumber = carousel.firstChild.id.slice(-1);
+    //Get the previous CardId 
+    const previousCardIdNumber = currentCardIdNumber == 1 ? 4 : parseInt(currentCardIdNumber) - 1;
+    const previousCard = document.getElementById("about"+previousCardIdNumber.toString())
+
+    //Display the content of the next CardId
+    while (carousel.firstChild) {
+      carousel.removeChild(carousel.firstChild);
+    }
+    carousel.appendChild(previousCard.cloneNode(true));
   })
 
 })
